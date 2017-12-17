30410c557a5c91e0115c3142e9d634f75e46cc66
Jake Shilling <shilling.jake@gmail.com>
2017-12-17 12:52:42 -0500

    Adding access flag constants

--- src/compilation_unit/include/j2c/compilation-unit-class.h
+++ src/compilation_unit/include/j2c/compilation-unit-class.h
@@ -9,6 +9,15 @@
 
 G_BEGIN_DECLS
 
+static const guint16 ACC_PUBLIC     = 0x0001;
+static const guint16 ACC_FINAL      = 0x0010;
+static const guint16 ACC_SUPER      = 0x0020;
+static const guint16 ACC_INTERFACE  = 0x0200;
+static const guint16 ACC_ABSTRACT   = 0x0400;
+static const guint16 ACC_SYNTHETIC  = 0x1000;
+static const guint16 ACC_ANNOTATION = 0x2000;
+static const guint16 ACC_ENUM       = 0x4000;
+
 #define J2C_TYPE_COMPILATION_UNIT_CLASS	j2c_compilation_unit_class_get_type ()
 G_DECLARE_FINAL_TYPE (J2cCompilationUnitClass, j2c_compilation_unit_class, J2C, COMPILATION_UNIT_CLASS, GObject)
 
