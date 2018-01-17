02d34772cc1e923167f367cc3eb5bec0c2132b42
Jake Shilling <shilling.jake@gmail.com>
2018-01-17 13:26:14 -0500

    Adding unit test structure

---
+++ tests/reader/zip-test.c
@@ -0,0 +1,26 @@
+#include <glib.h>
+#include <gio/gio.h>
+
+#include <locale.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <j2c/zip.h>
+
+static void
+zip_test_zip_new (void)
+{
+
+}
+
+int
+main (int argc, char *argv[])
+{
+  setlocale (LC_ALL, "");
+
+  g_test_init (&argc, &argv, NULL);
+
+  g_test_add_func ("/zip/zip-new", zip_test_new);
+
+  return g_test_run();
+}
