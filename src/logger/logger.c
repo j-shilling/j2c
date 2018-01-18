78fd2e02c368d35ded870bd7e384b8f6f70f81df
Jake Shilling <shilling.jake@gmail.com>
2018-01-18 10:36:24 -0500

    Fixing logger thread problem

--- src/logger/logger.c
+++ src/logger/logger.c
@@ -11,6 +11,7 @@ struct _Logger
 {
   J2cLoggerLevel level;
   GFile *file;
+  GMutex *mutex;
 };
 
 static struct _Logger logger = { J2C_LOGGER_LEVEL_WARNING, NULL };
@@ -32,24 +33,6 @@ static struct _Level levels_table[J2C_LOGGER_LEVEL_N_LEVELS] =
     { "[FATAL]:   ", TRUE  }
 };
 
-static void
-j2c_logger_file_write_callback (GObject *source_object,
-				GAsyncResult *res,
-				gpointer user_data)
-{
-  GOutputStream *ostream = G_OUTPUT_STREAM (source_object);
-
-  GError *error = NULL;
-  if (!g_output_stream_close (ostream, NULL, &error))
-    {
-      j2c_logger_set_file (NULL);
-      j2c_logger_warning ("Could not close log file: %s", error->message);
-      g_error_free (error);
-    }
-  g_object_unref (ostream);
-  g_free (user_data);
-}
-
 void
 j2c_logger_set_level (J2cLoggerLevel const level)
 {
@@ -69,9 +52,64 @@ j2c_logger_set_file (GFile *file)
   g_free (path);
 }
 
+typedef struct
+{
+  FILE *out;
+  gchar *text;
+} J2cWriteArgs;
+
+static gpointer
+j2c_logger_write (gpointer data)
+{
+  J2cWriteArgs *args = data;
+  g_mutex_lock (logger.mutex);
+
+  g_fprintf (args->out, "%s", args->text);
+  if (logger.file)
+    {
+      GError *error = NULL;
+      GFileOutputStream *ostream = g_file_append_to (logger.file,
+						     G_FILE_CREATE_NONE,
+						     NULL,
+						     &error);
+      if (!ostream)
+        {
+          j2c_logger_set_file (NULL);
+          j2c_logger_warning ("Could not log to file: %s", error->message);
+          g_error_free (error);
+        }
+      else
+        {
+          gsize count = strlen (args->text);
+          g_output_stream_write (G_OUTPUT_STREAM(ostream),
+				 args->text,
+                           count,
+                           NULL,
+                           &error);
+	  if (error)
+	    j2c_logger_warning ("%s", error->message);
+	  g_object_unref (ostream);
+        }
+
+    }
+
+
+  g_mutex_unlock (logger.mutex);
+  g_free (args->text);
+  g_free (args);
+
+  return NULL;
+}
+
 void
 j2c_logger_log (J2cLoggerLevel const level, gchar const *const __fmt, ...)
 {
+  if (logger.mutex == NULL)
+    {
+      logger.mutex = g_malloc (sizeof (GMutex));
+      g_mutex_init (logger.mutex);
+    }
+
   if (level < logger.level)
     return;
 
@@ -91,44 +129,20 @@ j2c_logger_log (J2cLoggerLevel const level, gchar const *const __fmt, ...)
 
   g_free (msg);
 
-  g_fprintf (out, "%s", text);
+  J2cWriteArgs *write_args = g_malloc (sizeof (J2cWriteArgs));
+  write_args->text = text;
+  write_args->out = out;
 
-  if (logger.file)
-    {
-      GError *error = NULL;
-      GFileOutputStream *ostream = g_file_append_to (logger.file,
-						     G_FILE_CREATE_NONE,
-						     NULL,
-						     &error);
-      if (!ostream)
-        {
-          j2c_logger_set_file (NULL);
-          j2c_logger_warning ("Could not log to file: %s", error->message);
-          g_error_free (error);
-        }
-      else
-        {
-          gchar *buffer = g_strdup (text);
-          gsize count = strlen (buffer);
-          g_output_stream_write_async (G_OUTPUT_STREAM(ostream),
-                           buffer,
-                           count,
-                           G_PRIORITY_DEFAULT,
-                           NULL,
-                           j2c_logger_file_write_callback,
-                           buffer);
-        }
-
-    }
-
-  g_free (text);
+  GThread *thread = g_thread_new ("j2c-logger-write", j2c_logger_write, write_args);
 }
 
-void
-j2c_logger_heading (gchar const *const heading)
+static gpointer
+j2c_logger_write_heading (gpointer data)
 {
   static const guint line_length = 64;
+  gchar const *const heading = data;
 
+  g_mutex_lock (logger.mutex);
   g_printf ("\n");
   g_printf ("\n");
   for (int i = 0; i < line_length; i ++)
@@ -160,4 +174,18 @@ j2c_logger_heading (gchar const *const heading)
     g_printf ("*");
   g_printf ("\n");
   g_printf ("\n");
+  g_mutex_unlock (logger.mutex);
+
+  return NULL;
+}
+void
+j2c_logger_heading (gchar const *const heading)
+{
+  if (logger.mutex == NULL)
+    {
+      logger.mutex = g_malloc (sizeof (GMutex));
+      g_mutex_init (logger.mutex);
+    }
+
+  GThread *thread = g_thread_new ("j2c-logger-write", j2c_logger_write_heading, (gpointer) heading);
 }
