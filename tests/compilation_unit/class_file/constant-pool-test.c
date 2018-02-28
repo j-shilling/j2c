c3aa89a92557b0e8a794688f25de491011ead05b
Jake Shilling <shilling.jake@gmail.com>
2018-02-28 14:39:30 -0500

    Parsing XML files for unit testing

--- tests/compilation_unit/class_file/constant-pool-test.c
+++ tests/compilation_unit/class_file/constant-pool-test.c
@@ -5,9 +5,112 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <locale.h>
+#include <math.h>
 
 #include <j2c/constant-pool.h>
 #include <j2c/constant-pool-item.h>
+
+typedef struct
+{
+
+  gchar *type;
+
+  guint16 name_index;
+  guint16 class_index;
+  guint16 name_and_type_index;
+  guint16 string_index;
+  gint32 int_value;
+  gfloat float_value;
+  gint64 long_value;
+  gdouble double_value;
+  guint16 descriptor_index;
+  gchar *utf8_value;
+  guint8 reference_kind;
+  guint16 reference_index;
+  guint16 bootstrap_method_attr_index;
+
+} ConstantPoolItem;
+
+typedef struct
+{
+  gchar *class_file_path;
+  ConstantPoolItem **arr;
+  gsize len;
+} ConstantPool;
+
+static ConstantPoolItem *
+constant_pool_item_alloc (void)
+{
+  return g_malloc0 (sizeof (ConstantPoolItem));
+}
+
+static void
+constant_pool_item_free (ConstantPoolItem *item)
+{
+  if (item)
+    {
+      if (item->type)
+	g_free (item->type);
+      if (item->utf8_value)
+	g_free (item->utf8_value);
+      g_free (item);
+    }
+}
+
+static void
+constant_pool_free (gpointer data)
+{
+  ConstantPool *pool = data;
+  if (pool)
+    {
+      if (pool->class_file_path)
+	g_free (pool->class_file_path);
+      if (pool->arr)
+	{
+	  for (gint i = 0; i < pool->len; i++)
+	    if (pool->arr[i])
+	      constant_pool_item_free (pool->arr[i]);
+	  g_free (pool->arr);
+	}
+      g_free (pool);
+    }
+}
+
+static ConstantPool *
+constant_pool_alloc (void)
+{
+  ConstantPool *pool = g_malloc0 (sizeof (ConstantPool));
+  return pool;
+}
+
+static long
+str_as_long (const gchar *text, GError **error)
+{
+  long ret = strtol (text, NULL, 10);
+
+  if (ret == LONG_MIN || ret == LONG_MAX)
+    {
+      g_set_error (error, 1, 1, "Cannot parse \'%s\' as a number", text);
+      return 0;
+    }
+
+  return ret;
+}
+
+static double
+str_as_double (const gchar *text, GError **error)
+{
+  if (!g_strcmp0 (text, "Infinity"))
+    return INFINITY;
+  else if (!g_strcmp0 (text, "-Infinity"))
+    return -INFINITY;
+  else if (!g_strcmp0 (text, "NaN"))
+    return NAN;
+
+  double ret = strtod (text, NULL);
+
+  return ret;
+}
 
 static void
 parse_start_element (GMarkupParseContext *context,
@@ -17,7 +120,74 @@ parse_start_element (GMarkupParseContext *context,
 		     gpointer user_data,
 		     GError **error)
 {
+  ConstantPool *pool = user_data;
 
+  /* parse <constant_pool> */
+  if (g_strcmp0 (element_name, "constant_pool") == 0)
+    {
+      for (int i = 0; attribute_names[i]; i++)
+	{
+	  if (!g_strcmp0 (attribute_names[i], "constant_pool"))
+	    {
+	      if (pool->class_file_path)
+		g_free (pool->class_file_path);
+	      pool->class_file_path = g_strdup (attribute_values[i]);
+	      break;
+	    }
+	}
+      return;
+    }
+
+  /* parse a child of <constant_pool> */
+  const GSList *stack = g_markup_parse_context_get_element_stack (context);
+  if (stack && stack->next && !g_strcmp0 (stack->next->data, "constant_pool"))
+    {
+      gchar **attrs = g_strsplit (element_name, "_", -1);
+      if (g_strv_length (attrs) != 2)
+	{
+	  g_strfreev (attrs);
+	  g_set_error (error, 1, 1, "Cannot parse item tag name \'%s\'", element_name);
+	  return;
+	}
+
+      GError *tmp_error = NULL;
+      long index = str_as_long (attrs[1], &tmp_error);
+      if (tmp_error)
+	{
+	  g_strfreev (attrs);
+	  g_propagate_error (error, tmp_error);
+	  return;	
+	}
+
+      ConstantPoolItem *item = NULL;
+      if (index < pool->len)
+	item = pool->arr[index];
+      if (!item)
+	{
+	  item = constant_pool_item_alloc ();
+	  if (index >= pool->len)
+	    {
+	      gsize len = index + 1;
+	      ConstantPoolItem **arr = g_malloc0 (len * sizeof (ConstantPoolItem *));
+	      for (gint i = 0; i < pool->len; i++)
+		if (pool->arr[i])
+		  arr[i] = pool->arr[i];
+
+	      g_free (pool->arr);
+	      pool->arr = arr;
+	      pool->len = len;
+	    }
+	  pool->arr[index] = item;
+	}
+
+      if (item->type)
+	g_free (item->type);
+
+      item->type = g_strdup (attrs[0]);
+      g_strfreev (attrs);
+
+      return;
+    }
 }
 
 static void
@@ -27,7 +197,83 @@ parse_text (GMarkupParseContext *context,
 	    gpointer user_data,
 	    GError **error)
 {
+  ConstantPool *pool = user_data;
+  const GSList *stack = g_markup_parse_context_get_element_stack (context);
+  if (!g_strcmp0 (stack->data, "constant_pool"))
+    /* Ignore <constant_pool> nodes */
+    return;
+  if (stack->next && !g_strcmp0 (stack->next->data, "constant_pool"))
+    /* Ignore children of <constant_pool> nodes */
+    return;
+  if (stack->next && stack->next->next && !g_strcmp0 (stack->next->next->data, "constant_pool"))
+    {
+      /* Handle grandchildren of <constant_pool> nodes */
+      const gchar *parent_name = stack->next->data;
+      const gchar *element_name = stack->data;
 
+      gchar **attrs = g_strsplit (parent_name, "_", -1);
+      if (g_strv_length (attrs) != 2)
+	{
+	  g_strfreev (attrs);
+	  g_set_error (error, 1, 1, "Cannot parse item tag name \'%s\'", parent_name);
+	  return;
+	}
+
+      GError *tmp_error = NULL;
+      long index = str_as_long (attrs[1], &tmp_error);
+      if (index == LONG_MIN || index == LONG_MAX)
+	{
+	  g_strfreev (attrs);
+	  g_propagate_error (error, tmp_error);
+	  return;	
+	}
+      g_strfreev (attrs);
+
+      if (index >= pool->len)
+	{
+	  g_set_error (error, 1, 1, "%ld is not a valid index in arr len %ld", index, pool->len);
+	  return;
+	}
+
+      ConstantPoolItem *item = pool->arr[index];
+      if (!item)
+	{
+	  g_set_error (error, 1, 1, "No item has been  placed at index %ld", index);
+	  return;
+	}
+
+      gchar *str = g_malloc0 (text_len);
+      snprintf (str, text_len, "%s", text);
+
+      if (!g_strcmp0 (element_name, "class_index"))
+	item->class_index = str_as_long (str, error);
+      else if (!g_strcmp0 (element_name, "name_and_type_index"))
+	item->name_and_type_index = str_as_long (str, error);
+      else if (!g_strcmp0 (element_name, "string_index"))
+	item->string_index = str_as_long (str,error);
+      else if (!g_strcmp0 (element_name, "value") && !g_strcmp0 (item->type, "Integer"))
+	item->int_value = str_as_long (str, error);
+      else if (!g_strcmp0 (element_name, "value") && !g_strcmp0 (item->type, "Long"))
+	item->long_value = str_as_long (str, error);
+      else if (!g_strcmp0 (element_name, "value") && !g_strcmp0 (item->type, "Float"))
+	item->float_value = str_as_double (str, error);
+      else if (!g_strcmp0 (element_name, "value") && !g_strcmp0 (item->type, "Double"))
+	item->double_value = str_as_double (str, error);
+      else if (!g_strcmp0 (element_name, "value") && !g_strcmp0 (item->type, "Utf8"))
+	item->utf8_value = g_strdup (str);
+      else if (!g_strcmp0 (element_name, "name_index"))
+	item->name_index = str_as_long (str, error);
+      else if (!g_strcmp0 (element_name, "descriptor_index"))
+	item->descriptor_index = str_as_long (str, error);
+      else if (!g_strcmp0 (element_name, "reference_kind"))
+	item->reference_kind = str_as_long (str, error);
+      else if (!g_strcmp0 (element_name, "reference_index"))
+	item->reference_index = str_as_long (str, error);
+      else if (!g_strcmp0 (element_name, "bootstrap_method_attr_index"))
+	item->bootstrap_method_attr_index = str_as_long (str, error);
+
+      g_free (str);
+   }
 }
 
 static void
@@ -124,7 +370,8 @@ test_constant_pool (gconstpointer user_data)
   GMarkupParser *parser = g_malloc0 (sizeof (GMarkupParser));
   parser->start_element = parse_start_element;
   parser->text = parse_text;
-  GMarkupParseContext *context = g_markup_parse_context_new (parser, 0, NULL, g_free);
+  ConstantPool *pool = constant_pool_alloc ();
+  GMarkupParseContext *context = g_markup_parse_context_new (parser, 0, pool, constant_pool_free);
 
   gsize text_len = 0;
   gchar *text = g_bytes_unref_to_data (bytes, &text_len);
@@ -133,6 +380,8 @@ test_constant_pool (gconstpointer user_data)
   g_free (text);
   if (error)
     goto error;
+
+  g_markup_parse_context_free (context);
 
 error:
   if (error)
--- tests/compilation_unit/class_file/constantpooldatagen.py
+++ tests/compilation_unit/class_file/constantpooldatagen.py
@@ -5,6 +5,7 @@ import os
 import re
 import subprocess
 import shutil
+import threading
 import xml.etree.ElementTree as ET
 
 class ConstantPoolItem:
@@ -31,7 +32,12 @@ class ConstantPoolItem:
             self.value = int (args[3].replace("l",""))
 
         elif self.type == "Float" or self.type == "Double":
-            self.value = float (args[3].replace("d","").replace("f",""))
+            if (args[3].startswith("Infinity")):
+                self.value = "Infinity"
+            elif (args[3].startswith ("-Infinity")):
+                self.values = "-Infinity"
+            else:
+                self.value = float (args[3].replace("d","").replace("f",""))
 
         elif self.type == "NameAndType":
             vals = args[3].split(':')
@@ -92,7 +98,7 @@ class ConstantPoolItem:
         return ret
 
     def attach (self, parent):
-        elmt = ET.SubElement (parent, self.type, { 'index':str(self.index) })
+        elmt = ET.SubElement (parent, self.type + "_" + str(self.index))
         attrs = dir (self)
         attrs.remove ('index')
         attrs.remove ('type')
@@ -102,6 +108,45 @@ class ConstantPoolItem:
             node.text = str (getattr (self, attr))
 
         return elmt
+
+class ParseThread (threading.Thread):
+
+    def __init__ (self, in_path, out_path):
+        threading.Thread.__init__ (self)
+        self.in_path = in_path
+        self.out_path = out_path
+
+    def run (self):
+        print ("Reading", self.in_path, "to", self.out_path)
+
+        result = subprocess.run (
+            ["javap", "-verbose", self.in_path],
+            stdin = subprocess.DEVNULL, stdout = subprocess.PIPE, stderr = subprocess.DEVNULL).stdout.decode ('utf-8')
+
+        cp = ET.Element ('constant_pool', { 'file': os.path.abspath (self.in_path) } )
+        tree = ET.ElementTree(cp)
+
+        inConstantPool = False
+        constantPool = [];
+        for line in result.splitlines():
+            if (inConstantPool):
+                if (len(line) > 0 and line[0] == '{'):
+                    inConstantPool = False
+                else:
+                    constantPool.append(line)
+            else:
+                inConstantPool = line.startswith("Constant pool")
+
+        for line in constantPool:
+            try:
+                item = ConstantPoolItem (line)
+                elmnt = item.attach (cp)
+
+            except ValueError as e:
+                print ("[WARNING] in ", path, ": ", e)
+
+        os.makedirs (os.path.dirname (self.out_path), exist_ok=True)
+        tree.write (self.out_path)
 
 jre = os.environ['JAVA_HOME']
 if os.path.isdir (os.path.join (jre, "jre")):
@@ -129,41 +174,18 @@ subprocess.run (["jar", "xvf", jt], cwd=rtdir, stdout = subprocess.DEVNULL)
 
 os.chdir (cwd)
 
+threads = []
 for root, dirs, files in os.walk (rtdir, followlinks = True):
     for name in files:
         if not name.endswith (".class") or name == "package-info.class":
             continue
 
-        print ("Parsing ", name)
+        in_path = os.path.join (root, name)
+        out_path = os.path.join (outdir, name.replace(".class", ".constant_pool.xml"))
+        
+        thread = ParseThread (in_path, out_path)
+        thread.run()
+        threads.append (thread)
 
-        path = os.path.join (root, name)
-        out = os.path.join (outdir, name.replace(".class", ".constant_pool.xml"))
-
-        result = subprocess.run (
-            ["javap", "-verbose", path],
-            stdin = subprocess.DEVNULL, stdout = subprocess.PIPE, stderr = subprocess.DEVNULL).stdout.decode ('utf-8')
-
-        cp = ET.Element ('constant_pool', { 'file': os.path.abspath (path) } )
-        tree = ET.ElementTree(cp)
-
-        inConstantPool = False
-        constantPool = [];
-        for line in result.splitlines():
-            if (inConstantPool):
-                if (line[0] == '{'):
-                    inConstantPool = False
-                else:
-                    constantPool.append(line)
-            else:
-                inConstantPool = line.startswith("Constant pool")
-
-        for line in constantPool:
-            try:
-                item = ConstantPoolItem (line)
-                elmnt = item.attach (cp)
-
-            except ValueError as e:
-                print ("[WARNING] in ", path, ": ", e)
-
-        os.makedirs (os.path.dirname (out), exist_ok=True)
-        tree.write (out)
+for thread in threads:
+    thread.join()
